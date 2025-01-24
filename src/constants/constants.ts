23d6ca3aefb34db0e06bbfb75e9fcb7ee9f30a26
Taiki-sub <akmatsutaiki@gmail.com>
2025-01-24 15:44:14 +0900

    levelを調整

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,51 +1,51 @@
 export const kanbans = {
   上中: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/64",
-    level: 2
+    level: 3
   },
   大森: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/68",
-    level: 2
+    level: 3
   },
   亀岡: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/54",
-    level: 1
+    level: 2
   },
   野口と: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/58",
-    level: 4
+    level: 1
   },
   飯田: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/65",
-    level: 4
+    level: 1
   },
   川﨑: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/52",
-    level: 4
+    level: 1
   },
   赤松: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/63",
-    level: 4
+    level: 1
   },
   堀尾: { 
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/49", 
-    level: 4 
+    level: 1 
   },
   野口は: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/62",
-    level: 3
+    level: 4
   },
   橋本: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/59",
-    level: 3
+    level: 4
   },
   豊嶋: {
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/61",
-    level: 3
+    level: 4
   },
   吉田: { 
     url: "https://github.com/orgs/Wakayama-SocSEL/projects/60", 
-    level: 3
+    level: 4
   },
 }
 
